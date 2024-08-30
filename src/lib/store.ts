aed080923d029750d3b3426e0a8582655d16cbd7
Ꮢαყααɳ <rayaannsn@gmail.com>
2024-08-31 02:17:34 +0530

    feat: add position

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -3,9 +3,13 @@ import { create } from "zustand";
 export type Store = {
     count: number;
     setCount: (count: number) => void;
+    userID: string;
+    setUserID: (userID: string) => void;
 };
 
 export const useStore = create<Store>((set) => ({
     count: 0,
     setCount: (count: number) => set({ count }),
+    userID: "",
+    setUserID: (userID: string) => set({ userID }),
 }));
