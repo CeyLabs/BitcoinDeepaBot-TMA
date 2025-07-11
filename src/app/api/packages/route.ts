5cc403c9a4a60c4405e3ff89822edc4e87fb20ac
Nimsara <udananimsara@gmail.com>
2025-07-11 21:29:06 +0530

    fix: Ensure cache is set to "no-cache" in API package fetch request

--- src/app/api/packages/route.ts
+++ src/app/api/packages/route.ts
@@ -7,6 +7,7 @@ export async function GET() {
             headers: {
                 "Content-Type": "application/json",
             },
+            cache: "no-cache"
         });
 
         const packages = await response.json();
